0142756b0d83da4e744a11b7a97afc41a6efdb24
Daniel Schwarz <d.schwarz@curasystems.de>
2013-10-29 14:48:08 +0100

    added script to initialize config

---
+++ initialize.js
@@ -0,0 +1,48 @@
+var readline = require('readline');
+var async = require('async');
+var fs = require('fs');
+
+module.exports = function(){
+
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  var config = {};
+
+  async.series([
+    function(cb) {
+      rl.question('Which port do you want the blog to listen on? ', function(port){
+        config.blogPort = port;
+        cb(null);
+      });
+    },
+    function(cb){
+      rl.question('What is the url of your database? ', function(url){
+        config.databaseUrl = url;
+        cb(null);
+      });
+    },
+    function(cb){
+    rl.question('Admin username: ', function(name){
+      config.username = name;
+      cb(null);
+    });
+    },
+    function(cb){
+      rl.question('Admin password: ', function(name){
+        config.password = name;
+        rl.close();
+        cb(null);
+      });
+    }
+
+    ], function(err){
+      console.log(config);
+      config = JSON.stringify(config);
+      config = JSON.beautify(config);
+      fs.writeFileSync('./blog-config', config);
+    });
+
+}();
