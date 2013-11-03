d444734c0a5f73fc5f281ceedca6114cbc486188
Adrian Ortmann <adrian.ortmann@online.de>
2013-11-03 18:09:16 +0100

    added a database connection but it just saves every time a user with admin and admin password :D

--- index.js
+++ index.js
@@ -3,17 +3,20 @@
 var fs = require('fs');
 var initializeBlog = require('./lib/initialize');
 var setupBlog = require('./lib/setup-blog');
+var database = require('./lib/database');
 
 
 
 module.exports.start = function(settings) {
+  database.connect();
+
   if (fs.existsSync('./blog.config') === false) {
-    console.log('no config file - initializing')
+    console.log('no config file - initializing');
     initializeBlog(function(){
       setupBlog(settings);
     });
   } else {
-    console.log('config found - starting blog')
+    console.log('config found - starting blog');
     setupBlog(settings);
-  };
-}
+  }
+};
