3f07b145efae7c9297ab9755871768606492a427
Adrian Ortmann <adrian.ortmann@online.de>
2013-11-10 12:53:52 +0100

    last changes before publishing on github. still many errors

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ var fs = require('fs');
 var initializeBlog = require('./lib/initialize');
 var Database = require('./lib/database');
 var webserver = require('./lib/webserver');
+var htmlBuilder = require('./lib/html-builder');
 
 module.exports.start = function(settings, mailTransport) {
+  htmlBuilder.buildNavbar(settings.navbar);
+
   if (fs.existsSync('./blog.config') === false) {
     console.log('no config file - initializing');
     initializeBlog(function(database){
-      webserver(settings, mailTransport, database);
+      webserver(mailTransport, database);
     });
   } else {
     console.log('config found - starting blog');
@@ -21,7 +24,7 @@ module.exports.start = function(settings, mailTransport) {
       }
       var blogConfig = JSON.parse(data);
       var database = new Database(blogConfig.databaseUser, blogConfig.databasePassword, blogConfig.databaseUrl);
-      webserver(settings, mailTransport, database);
+      webserver(mailTransport, database);
     });
   }
 };
--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -32,24 +32,59 @@ function buildJson(emailAddress, name, topic, text) {
   return json;
 }
 
-function sendContactInformationToServer(url, json, navigateTo) {
-  $.ajax({
-    type : 'POST',
-    url : url,
-    contentType : 'application/json; charset=utf-8',
-    dataType : 'json',
-    async : false,
-    data : JSON.stringify(json),
-    success : function(data) {
-      if(data == true) {
-        $('#blog-navbar').append('<li id="adminArea">' +
-          '<a onclick="navigateToRequestedID(\'adminArea\');">Admin-Panel</a></li>');
+function sendContactInformationToServer(url, json, navigateTo){
 
-        navigateToRequestedID(navigateTo);
-      }
-    }
+  var user = json.username;
+  var password = json.password;
+  alert('after pass');
+  $.get('/userusername', function(jd) {
+    alert('fdsg');
   });
+  // $.getJSON(
+  //         '/user?username',
+  //         //'user?username=' + user + "&password=" + password,
+  //         function(data) {
+  //           alert('inside...');
+  //           if (jQuery.isEmptyObject(data)) {
+  //             //setErrorClassForInputFields('#loginEmailInput');
+  //             //setErrorClassForInputFields('#loginPasswordInput');
+  //             return;
+  //           }
+  //           sessionStorage.setItem('username', data.userName);
+  //           sessionStorage.setItem('password', password);
+  //           $("#navigationTabsList")
+  //               .append(
+  //                   '<li id="myRegistration">'
+  //                       + '<a onclick="navigateToRequestedID(\'myRegistration\');">Meine Anmeldung</a></li>');
+  //           navigateToRequestedID('myRegistration');
+  //           hideOrShowDivs("#navbarRightWhenLoggedIn", true);
+  //           hideOrShowDivs("#navbarRightToLogin", false);
+  //           $('#loginEmailInput').val('');
+  //           $('#loginPasswordInput').val('');
+  //           $('#username').text(email); 
+  //         });
+
 }
+
+
+// function sendContactInformationToServer(url, json, navigateTo) {
+//   $.ajax({
+//     type : 'POST',
+//     url : url,
+//     contentType : 'application/json; charset=utf-8',
+//     dataType : 'json',
+//     async : false,
+//     data : JSON.stringify(json),
+//     success : function(data) {
+//       if(data == true) {
+//         $('#blog-navbar').append('<li id="adminArea">' +
+//           '<a onclick="navigateToRequestedID(\'adminArea\');">Admin-Panel</a></li>');
+//         navigateToRequestedID(navigateTo);
+//         alert('break');
+//       }
+//     }
+//   });
+// }
 
 
 // $("#navigationTabsList")
