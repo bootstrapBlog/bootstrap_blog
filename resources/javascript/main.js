b9775b087245427dc71779b361385faf95d70668
Adrian Ortmann <adrian.ortmann@online.de>
2013-11-10 17:02:30 +0100

    login works now

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -23,7 +23,7 @@ function getFormFieldInformation(form, inputField) {
 
 function buildJson(emailAddress, name, topic, text) {
   var json = {
-    "emailAddress": emailAddress,
+    // "emailAddress": emailAddress,
     "name": name,
     "topic": topic,
     "text": text
@@ -36,61 +36,23 @@ function sendContactInformationToServer(url, json, navigateTo){
 
   var user = json.username;
   var password = json.password;
-  alert('after pass');
-  $.get('/userusername', function(jd) {
-    alert('fdsg');
-  });
-  // $.getJSON(
-  //         '/user?username',
-  //         //'user?username=' + user + "&password=" + password,
-  //         function(data) {
-  //           alert('inside...');
-  //           if (jQuery.isEmptyObject(data)) {
-  //             //setErrorClassForInputFields('#loginEmailInput');
-  //             //setErrorClassForInputFields('#loginPasswordInput');
-  //             return;
-  //           }
-  //           sessionStorage.setItem('username', data.userName);
-  //           sessionStorage.setItem('password', password);
-  //           $("#navigationTabsList")
-  //               .append(
-  //                   '<li id="myRegistration">'
-  //                       + '<a onclick="navigateToRequestedID(\'myRegistration\');">Meine Anmeldung</a></li>');
-  //           navigateToRequestedID('myRegistration');
-  //           hideOrShowDivs("#navbarRightWhenLoggedIn", true);
-  //           hideOrShowDivs("#navbarRightToLogin", false);
-  //           $('#loginEmailInput').val('');
-  //           $('#loginPasswordInput').val('');
-  //           $('#username').text(email); 
-  //         });
+  var authData = {
+    user: user,
+    password: password
+  };
 
+  $.getJSON('/auth', authData, function(data) {
+    if(jQuery.isEmptyObject(data)) {
+      return ;
+    }
+    $("#blog-navbar").append('<li id="adminArea">' +
+      '<a onclick="navigateToRequestedID(\'adminArea\');">Admin-Panel</a></li>');
+    navigateToRequestedID('adminArea');
+  });
 }
 
-
-// function sendContactInformationToServer(url, json, navigateTo) {
-//   $.ajax({
-//     type : 'POST',
-//     url : url,
-//     contentType : 'application/json; charset=utf-8',
-//     dataType : 'json',
-//     async : false,
-//     data : JSON.stringify(json),
-//     success : function(data) {
-//       if(data == true) {
-//         $('#blog-navbar').append('<li id="adminArea">' +
-//           '<a onclick="navigateToRequestedID(\'adminArea\');">Admin-Panel</a></li>');
-//         navigateToRequestedID(navigateTo);
-//         alert('break');
-//       }
-//     }
-//   });
-// }
-
-
-// $("#navigationTabsList")
-//                 .append(
-//                     '<li id="myRegistration">'
-//                         + '<a onclick="navigateToRequestedID(\'myRegistration\');">Meine Anmeldung</a></li>');
-//             navigateToRequestedID('myRegistration');
-//             hideOrShowDivs("#navbarRightWhenLoggedIn", true);
-//             hideOrShowDivs("#navbarRightToLogin", false);
+function realiseEnterButton(e) {
+  var charCode = (typeof e.which === "13") ? e.which : e.keyCode;
+  if(charCode === 13)
+    getAuthenticationValues();
+}
